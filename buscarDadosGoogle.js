b68e173796790a5f67bd99604e3f0161d734a705
Sniperbinario <hiago.vinicius.b@gmail.com>
2025-05-16 23:44:26 -0300

    Update buscarDadosGoogle.js

--- buscarDadosGoogle.js
+++ buscarDadosGoogle.js
@@ -29,11 +29,11 @@ async function buscarDadosGoogle(jogo) {
     return {
       fonte: "Google",
       textoCompleto: texto,
-      escanteios: (texto.match(/Escanteios\\s+(\\d+)/i) || [])[1] || 'N/D',
-      chutes: (texto.match(/Chutes\\s+(\\d+)/i) || [])[1] || 'N/D',
-      cartoes: (texto.match(/Cartões amarelos\\s+(\\d+)/i) || [])[1] || 'N/D',
-      gols: (texto.match(/(\\d+)\\s+x\\s+(\\d+)/i) || []).slice(1, 3).join('x') || 'N/D',
-      tempo: (texto.match(/\\d+º tempo - \\d+ min/) || [])[0] || 'Em andamento',
+      escanteios: (texto.match(/Escanteios\s+(\d+)/i) || [])[1] || 'N/D',
+      chutes: (texto.match(/Chutes\s+(\d+)/i) || [])[1] || 'N/D',
+      cartoes: (texto.match(/Cartões amarelos\s+(\d+)/i) || [])[1] || 'N/D',
+      gols: (texto.match(/(\d+)\s+x\s+(\d+)/i) || []).slice(1, 3).join('x') || 'N/D',
+      tempo: (texto.match(/\d+º tempo - \d+ min/) || [])[0] || 'Em andamento',
     };
   });
 
